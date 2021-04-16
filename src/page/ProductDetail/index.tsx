3a7e98dfa50b3729a5f8f09dcaaadd1a9709f19e
Yeongin Park <pyi7628@gmail.com>
2021-04-16 18:48:13 +0900

    feat: Add form handler (#21.2)

--- src/page/ProductDetail/index.tsx
+++ src/page/ProductDetail/index.tsx
@@ -11,35 +11,52 @@ import {
   FormText,
 } from 'reactstrap';
 import './style.css';
-import { Dummy } from '../../utils';
 
 // 임시
 import testImg from '../../assets/product_test.jpeg';
 
-interface Product {
-  id: number;
-  img: string;
+interface ProductInfo {
   name: string;
   price: string;
   stock: number;
-  rate: number;
-  createDt: string;
+  group: string;
+  desc: string;
+  descImg: string;
 }
 
 const ProductDetail: React.FC = () => {
-  // set state
+  // state & variable
+  const [submitValue, setSubmitValue] = useState({} as ProductInfo);
   const productsGroupList: Array<string> = [
     '폭신폭신 의자',
     '안폭신폭신 의자',
     '물침대',
     '돌침대',
   ];
-  const productGroup = productsGroupList.map(group => <option>{group}</option>);
+  const tempValue = {} as any; // form value 임시 저장
+
+  // comp
+  const productGroup = productsGroupList.map(group => (
+    <option key={group}>{group}</option>
+  ));
+
+  // method
+  const submitProductInfo = () => {
+    alert('submit Data!');
+  };
+
+  const handleOnChange = (evt: React.FormEvent<HTMLInputElement>) => {
+    const target = evt.target as HTMLInputElement;
+    tempValue[target.name] = target.value;
+    console.log(tempValue, target.value);
+  };
 
   return (
     <Container>
       <Row>
-        <Button>Submit</Button>
+        <Button type="submit" form="productPrimary">
+          Submit
+        </Button>
       </Row>
       <Row>
         <Col xs="5">
@@ -62,23 +79,26 @@ const ProductDetail: React.FC = () => {
           </div>
         </Col>
         <Col xs="7">
-          {' '}
-          <Form>
+          <Form id="productPrimary" onSubmit={submitProductInfo}>
             <FormGroup>
               <Label for="productName">상품명</Label>
               <Input
                 type="text"
-                name="productName"
+                name="name"
                 id="productName"
-                placeholder="제목에 보일 상품 이름을 적어주세요."
+                value={submitValue.name}
+                onChange={handleOnChange}
+                placeholder="상품 이름을 적어주세요."
               />
             </FormGroup>
             <FormGroup>
               <Label for="productPrice">상품 가격</Label>
               <Input
                 type="number"
-                name="productPrice"
+                name="price"
                 id="productPrice"
+                value={submitValue.price}
+                onChange={handleOnChange}
                 placeholder="판매 가격을 적어주세요."
               />
             </FormGroup>
@@ -86,24 +106,38 @@ const ProductDetail: React.FC = () => {
               <Label for="productStock">재고</Label>
               <Input
                 type="number"
-                name="productStock"
+                name="stock"
                 id="productStock"
+                value={submitValue.stock}
+                onChange={handleOnChange}
                 placeholder="판매 수량을 적어주세요."
               />
             </FormGroup>
             <FormGroup>
               <Label for="productGroup">그룹</Label>
-              <Input type="select" name="productGroup" id="productGroup">
+              <Input
+                type="select"
+                name="group"
+                id="productGroup"
+                value={submitValue.name}
+                onChange={handleOnChange}
+              >
                 {productGroup}
               </Input>
             </FormGroup>
             <FormGroup>
-              <Label for="productText">상품 상세 설명</Label>
-              <Input type="textarea" name="productText" id="productText" />
+              <Label for="productDesc">상품 상세 설명</Label>
+              <Input
+                type="textarea"
+                name="desc"
+                id="productDesc"
+                maxLength={500}
+                placeholder="상품에 대한 간단한 설명을 적어주세요 :)"
+              />
             </FormGroup>
             <FormGroup>
-              <Label for="productExFile">상품 상세 이미지</Label>
-              <Input type="file" name="productExFile" id="productExFile" />
+              <Label for="productDescImg">상품 상세 이미지</Label>
+              <Input type="file" name="descImg" id="productDescImg" />
               <FormText color="muted">
                 상품 상세 설명 이미지를 넣어주세요 :)
               </FormText>
