ee1a4736301dc5758e6b81ef01ab2c5f5a54528e
Yeongin Park <pyi7628@gmail.com>
2021-04-20 23:14:24 +0900

    test: Make AdjustPage render test (#28.1)

---
+++ src/page/adjustPage/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import AdjustPage from './index';
+
+describe('Product Page', () => {
+  it('renders Product page', () => {
+    render(<AdjustPage />);
+  });
+});
