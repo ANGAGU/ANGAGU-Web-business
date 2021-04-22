eae37ff4151a5480316d2ab11620d18f975b3386
Yeongin Park <pyi7628@gmail.com>
2021-04-22 20:54:23 +0900

    feat: Make validation tool of email and pw (#17.2)

---
+++ src/utils/validation.ts
@@ -0,0 +1,14 @@
+const Validation = {
+  isEmail: (value: string): boolean => {
+    // eslint-disable-next-line no-useless-escape
+    const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    return regExp.test(value);
+  },
+  isPassword: (value: string): boolean => {
+    // eslint-disable-next-line no-useless-escape
+    const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+    return regExp.test(value);
+  },
+};
+
+export default Validation;
