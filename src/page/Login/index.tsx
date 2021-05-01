7574546a3b1db54b44455e2c24993f3d488233c0
Yeongin Park <pyi7628@gmail.com>
2021-05-02 01:27:33 +0900

    feat: Modifiy style (#17.2)

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -3,14 +3,13 @@ import { Route, useRouteMatch } from 'react-router-dom';
 import { LoginTemplate, SignupTemplate } from 'components/template';
 import { bodyStyle } from './style';
 
-
 const Login: React.FC = () => {
   const { path } = useRouteMatch();
   return (
     <div style={bodyStyle}>
       <Route exact path={`${path}/Signup`} component={SignupTemplate} />
       <Route exact path={`${path}`} component={LoginTemplate} />
-      <Route exact path={`${path}/:Admin`} component={LoginTemplate} />
+      <Route exact path={`${path}/Admin/:Admin`} component={LoginTemplate} />
     </div>
   );
 };
