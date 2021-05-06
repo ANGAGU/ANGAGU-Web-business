af144227cd789db728f2ab795a75d549f80639c0
Yeongin Park <pyi7628@gmail.com>
2021-05-06 14:00:31 +0900

    test: Fix test props error (#33.1)

--- src/components/molecules/CompanyFilter/index.test.tsx
+++ src/components/molecules/CompanyFilter/index.test.tsx
@@ -1,12 +1,21 @@
 import { fireEvent, render } from '@testing-library/react';
 import CompanyFilter from './index';
 
+const companyFilterProps = {
+  selectCompany: () => {
+    console.log('dummy');
+  },
+};
 describe('Product Page', () => {
   it('renders Product page', () => {
-    render(<CompanyFilter />);
+    render(
+      <CompanyFilter selectCompanyFunc={companyFilterProps.selectCompany} />,
+    );
   });
   it('change user company name input', () => {
-    const { getByLabelText } = render(<CompanyFilter />);
+    const { getByLabelText } = render(
+      <CompanyFilter selectCompanyFunc={companyFilterProps.selectCompany} />,
+    );
     const input = getByLabelText('회사명') as HTMLInputElement;
     fireEvent.change(input, {
       target: {
--- src/components/molecules/DateSelector/index.test.tsx
+++ src/components/molecules/DateSelector/index.test.tsx
@@ -1,11 +1,18 @@
 import { fireEvent, render } from '@testing-library/react';
 import DateSelector from './index';
 
+const dateSelectorProps = {
+  selectDate: () => {
+    console.log('dummy');
+  },
+};
 describe('Product Page', () => {
   it('renders Product page', () => {
-    render(<DateSelector />);
+    render(<DateSelector selectDateFunc={dateSelectorProps.selectDate} />);
   });
   it('change user company name input', () => {
-    const { getByLabelText } = render(<DateSelector />);
+    const { getByLabelText } = render(
+      <DateSelector selectDateFunc={dateSelectorProps.selectDate} />,
+    );
   });
 });
