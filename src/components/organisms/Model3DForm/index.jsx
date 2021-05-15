a9a6607eb3abc08725ab2f4bd8db6da1ebdb7702
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-05-16 01:56:26 +0900

    feat: 기업 3D 모델 미리보기 기능 수정
    Model3Dform 틀 수정

--- src/components/organisms/Model3DForm/index.jsx
+++ src/components/organisms/Model3DForm/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
-import { Modal3D } from '../../molecules';
+import { View3DModal } from '../../molecules';
 
 const Model3DForm = () => {
   return (
     <div className="product-3d-model">
       <h4>상품 3D 모델</h4>
-      <Modal3D />
+      <View3DModal />
     </div>
   );
 };
