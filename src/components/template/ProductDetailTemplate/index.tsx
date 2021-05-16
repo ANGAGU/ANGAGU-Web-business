bac4f1f93acfbb6da04535720c088e099b377b8a
Yeongin Park <pyi7628@gmail.com>
2021-05-16 20:29:44 +0900

    feat: Modify product submit value (#21.2)

--- src/components/template/ProductDetailTemplate/index.tsx
+++ src/components/template/ProductDetailTemplate/index.tsx
@@ -7,14 +7,15 @@ import './style.css';
 import testImg from '../../../assets/product_test.jpeg';
 import { Model3DForm } from '../../organisms';
 
-interface ProductInfo {
+type ProductInfo = {
   name: string;
   price: number;
   stock: number;
   group: string;
   desc: string;
   descImgUrl: string;
-}
+  delivery_charge: number;
+};
 
 type PreviewURL = {
   productImg1: string;
@@ -23,10 +24,13 @@ type PreviewURL = {
 
 const ProductDetailTemplate: React.FC = () => {
   // state & variable
-  const [submitValue, setSubmitValue] = useState({} as ProductInfo);
+  const [productValue, setProductValue] = useState({} as ProductInfo);
   const [descImg, setDescImg] = useState(null as File | null);
-  const [imgOrder, setImgOrder] = useState({});
+  const [thumbImg, setThumbImg] = useState(null as File | null);
+  const [detailImgs, setDetailImgs] = useState([] as Array<File | null>);
+  const [detailImgOrder, setDetailImgOrder] = useState({});
   const [previewURL, setPreviewURL] = useState({} as PreviewURL);
+
   const imgFormData = new FormData();
   const productsGroupList: Array<string> = ['폭신폭신 의자', '안폭신폭신 의자', '물침대', '돌침대'];
 
@@ -39,21 +43,25 @@ const ProductDetailTemplate: React.FC = () => {
 
     // await post(url, formData, config)
 
-    alert(`submit Data!${submitValue.name}`);
+    alert(`submit Data!${productValue.name}`);
   };
 
   const handleOnChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = evt.target as HTMLInputElement;
-    setSubmitValue({ ...submitValue, [name]: value });
+    setProductValue({ ...productValue, [name]: value });
   };
 
-  const handleOnChangeImg = (evt: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDescImg = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const files = evt.target.files as FileList;
     setDescImg(files[0]);
     handleOnChange(evt);
   };
-
-  const handleProductDetailImg = (evt: React.ChangeEvent<HTMLInputElement>) => {
+  const handleThumbImg = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const files = evt.target.files as FileList;
+    setThumbImg(files[0]);
+    handleOnChange(evt);
+  };
+  const handleDetailImg = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const { name } = evt.target as HTMLInputElement;
     const files = evt.target.files as FileList;
     // set preview
@@ -63,14 +71,19 @@ const ProductDetailTemplate: React.FC = () => {
     };
     reader.readAsDataURL(files[0]);
 
+    // set img file
+    const tempList = detailImgs;
+    tempList[Number(name) - 1] = files[0]; // eslint 구조분해할당 선호 에러 발생.. 구조분해로 어떻게 해야하나 확인해보기
+    setDetailImgs(tempList);
+
+    console.log(detailImgs);
     // set order
-    setImgOrder({ ...imgOrder, [files[0].name]: name });
-    imgFormData.append(name, files[0]);
-    console.log(imgOrder, imgFormData);
+    setDetailImgOrder({ ...detailImgOrder, [files[0].name]: Number(name) });
+    // imgFormData.append(name, files[0]);
   };
 
   const test = () => {
-    console.log(imgOrder);
+    console.log(detailImgOrder);
   };
 
   return (
@@ -87,12 +100,12 @@ const ProductDetailTemplate: React.FC = () => {
             <div className="product-img__content">
               <Form>
                 <FormGroup>
-                  <Input type="file" name="productImg1" onChange={handleProductDetailImg} />
+                  <Input type="file" name="1" onChange={handleDetailImg} />
                   {/* {previewURL.productImg1 && <img src={previewURL.productImg1} alt="" />} */}
-                  <Input type="file" name="productImg2" onChange={handleProductDetailImg} />
-                  <Input type="file" name="productImg3" onChange={handleProductDetailImg} />
-                  <Input type="file" name="productImg4" onChange={handleProductDetailImg} />
-                  <Input type="file" name="productImg5" onChange={handleProductDetailImg} />
+                  <Input type="file" name="2" onChange={handleDetailImg} />
+                  <Input type="file" name="3" onChange={handleDetailImg} />
+                  <Input type="file" name="4" onChange={handleDetailImg} />
+                  <Input type="file" name="5" onChange={handleDetailImg} />
                 </FormGroup>
                 <Button type="button" onClick={test}>
                   이미지 저장
@@ -111,7 +124,7 @@ const ProductDetailTemplate: React.FC = () => {
                 type="text"
                 name="name"
                 id="productName"
-                value={submitValue.name}
+                value={productValue.name}
                 onChange={handleOnChange}
                 placeholder="상품 이름을 적어주세요."
               />
@@ -122,9 +135,20 @@ const ProductDetailTemplate: React.FC = () => {
                 type="number"
                 name="price"
                 id="productPrice"
-                value={submitValue.price}
+                value={productValue.price}
                 onChange={handleOnChange}
                 placeholder="판매 가격을 적어주세요."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="deliveryCharge">배송비</Label>
+              <Input
+                type="number"
+                name="delivery_charge"
+                id="deliveryCharge"
+                value={productValue.price}
+                onChange={handleOnChange}
+                placeholder="배송비를 적어주세요."
               />
             </FormGroup>
             <FormGroup>
@@ -133,14 +157,14 @@ const ProductDetailTemplate: React.FC = () => {
                 type="number"
                 name="stock"
                 id="productStock"
-                value={submitValue.stock}
+                value={productValue.stock}
                 onChange={handleOnChange}
                 placeholder="판매 수량을 적어주세요."
               />
             </FormGroup>
             <FormGroup>
               <Label for="productGroup">그룹</Label>
-              <Input type="select" name="group" id="productGroup" value={submitValue.name} onChange={handleOnChange}>
+              <Input type="select" name="group" id="productGroup" value={productValue.group} onChange={handleOnChange}>
                 {productGroup}
               </Input>
             </FormGroup>
@@ -150,15 +174,20 @@ const ProductDetailTemplate: React.FC = () => {
                 type="textarea"
                 name="desc"
                 id="productDesc"
-                value={submitValue.desc}
+                value={productValue.desc}
                 onChange={handleOnChange}
                 maxLength={500}
                 placeholder="상품에 대한 간단한 설명을 적어주세요 :)"
               />
             </FormGroup>
             <FormGroup>
+              <Label for="productDescImg">상품 썸네일</Label>
+              <Input type="file" name="thumb_image" id="productDescImg" onChange={handleThumbImg} />
+              <FormText color="muted">상품 썸네일 이미지를 넣어주세요 :)</FormText>
+            </FormGroup>
+            <FormGroup>
               <Label for="productDescImg">상품 상세 이미지</Label>
-              <Input type="file" name="descImgUrl" id="productDescImg" onChange={handleOnChangeImg} />
+              <Input type="file" name="desc_image" id="productDescImg" onChange={handleDescImg} />
               <FormText color="muted">상품 상세 설명 이미지를 넣어주세요 :)</FormText>
             </FormGroup>
           </Form>
