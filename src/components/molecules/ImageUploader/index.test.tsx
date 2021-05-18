6a57160f9589127e44ade6ec90dc7307ac1c774f
Yeongin Park <pyi7628@gmail.com>
2021-05-18 15:23:30 +0900

    feat: Modify product detail css (#23.2)

--- src/components/molecules/ImageUploader/index.test.tsx
+++ src/components/molecules/ImageUploader/index.test.tsx
@@ -3,6 +3,6 @@ import ImageUploader from './index';
 
 describe('ImageUploader', () => {
   it('renders ImageUploader', () => {
-    render(<ImageUploader label="test" type="test" imgUrl="test" />);
+    render(<ImageUploader label="test" />);
   });
 });
