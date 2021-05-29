cf19c9e9b6bf01719d29bfb8335fa91d3a0d306b
Yeongin Park <pyi7628@gmail.com>
2021-05-29 16:35:23 +0900

    feat: Add date string share func

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -11,3 +11,19 @@ export const isPassword = (value: string): boolean => {
 export const isSame = (str1: string, str2: string): boolean => {
   return str1 === str2;
 };
+
+export const string2Date = (value: string) => {
+  const year = parseInt(value.substr(0, 4), 10);
+  const month = parseInt(value.substr(5, 2), 10);
+  return new Date(year, month - 1);
+};
+
+export const date2String = (value: Date) => {
+  const year = value.getFullYear();
+  const month = value.getMonth() + 1;
+  let result = '';
+  if (month < 10) result = `${year}-0${month}`;
+  else result = `${year}-${month}`;
+
+  return result;
+};
