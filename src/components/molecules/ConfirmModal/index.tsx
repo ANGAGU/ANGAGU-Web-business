28317d8e27369a30d2bc1938e91d653992737367
Yeongin Park <pyi7628@gmail.com>
2021-06-14 06:08:19 +0900

    feat: Make confirm modal counter

--- src/components/molecules/ConfirmModal/index.tsx
+++ src/components/molecules/ConfirmModal/index.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 import api from 'api';
+import { notify } from 'App';
 import './style.css';
 
 type ConfirmModalProps = {
@@ -18,6 +19,30 @@ const ConfirmModal: React.FC<ConfirmModalProps> & {
 } = ({ viewModal, phoneNumber, type, setAuthToken, toggleModal, setCode }) => {
   // const { viewModal, phoneNumber, setAuthToken } = props;
   const [verifyNumber, setVerifyNumber] = useState('' as string);
+  const [minutes, setMinutes] = useState(5 as number);
+  const [seconds, setSeconds] = useState(0 as number);
+
+  useEffect(() => {
+    setMinutes(5);
+    setSeconds(0);
+  }, [viewModal]);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
 
   const handleVerifyNumber = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = evt.target as HTMLInputElement;
@@ -35,9 +60,10 @@ const ConfirmModal: React.FC<ConfirmModalProps> & {
         code: verifyNumber,
       });
       if (status === 'success') {
-        alert('OK!');
+        notify('인증완료!');
         setAuthToken(data.token);
       } else {
+        notify('인증에 실패하였습니다.', 'error');
         console.log('fail for verify sms');
       }
     }
@@ -57,6 +83,9 @@ const ConfirmModal: React.FC<ConfirmModalProps> & {
         />
       </ModalBody>
       <ModalFooter>
+        <span className="counter">
+          {minutes}:{seconds}
+        </span>
         <Button color="primary" onClick={checkAuthNumber}>
           인증번호확인
         </Button>
