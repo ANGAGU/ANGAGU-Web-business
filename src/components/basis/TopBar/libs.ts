daf330ae812d4fc95b197004f02e5a0b4d8797f8
Yeongin Park <pyi7628@gmail.com>
2021-06-14 15:11:49 +0900

    fix: Fix logout history

--- src/components/basis/TopBar/libs.ts
+++ src/components/basis/TopBar/libs.ts
@@ -2,16 +2,16 @@ const TopBarLibs = {
   Logout: (e?: any, history?: any) => {
     if (localStorage.getItem('isAdmin') === 'true') {
       // 관리자이면
-      history.push(`/`);
+      history.push(`/Login`);
       localStorage.removeItem('isAdmin');
       localStorage.removeItem('token');
     } else {
       // 관리자 아니면
-      history.push(`/`);
+      history.push(`/Login`);
       localStorage.removeItem('isAdmin');
       localStorage.removeItem('token');
     }
-    if (history) history.push('/');
+    if (history) history.push('/Login');
   },
   goToRegister: (history: any, url: string) => {
     history.push(url);
